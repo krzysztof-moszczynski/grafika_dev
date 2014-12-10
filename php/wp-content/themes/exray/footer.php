0e6a424d8deaff8fdd5a59fa794d9e09be0ea961
krzysztof.moszczynski@gmail.com <krzysztof.moszczynski@gmail.com@e2376483-1fb2-310e-ae96-30bab79b6baf>
2014-12-10 12:25:32 +0000

    Commented EU banner in the footer section.

--- php/wp-content/themes/exray/footer.php
+++ php/wp-content/themes/exray/footer.php
@@ -3,7 +3,7 @@
 
 <div id="footer-container">
   <footer class="bottom-footer span11" role="contentinfo">
-    <ul id="eu_project_logos" class="clearfix">
+    <!--<ul id="eu_project_logos" class="clearfix">
       <li>
         <img src="/wp-content/themes/exray/images/kapital_ludzki_logo.png" alt="kapitał ludzki">
       </li>
@@ -13,7 +13,7 @@
       <li>
         <img src="/wp-content/themes/exray/images/eu_logo.png" alt="Europejski Fundusz Społeczny">
       </li>
-    </ul>
+    </ul>-->
     <div class="footer-widget-area">
       <div class="row">
         <?php get_sidebar('first-footer') ?>
